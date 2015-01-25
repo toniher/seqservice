4361bc491098a25832572acd60142d189338d14a
Toni Hermoso Pulido <toniher@cau.cat>
2015-01-25 15:02:14 +0100

    update

--- index.js
+++ index.js
@@ -40,8 +40,6 @@ app.set("io", io);
 
 var blastdbcmd = require('./routes/blastdbcmd.js');
 var blast = require('./routes/blast.js');
-var queries = require('./routes/queries.js');
-var wiki = require('./routes/wiki.js');
 
 // Landing
 app.get( basepath + '/', function(req, res){
@@ -50,22 +48,10 @@ app.get( basepath + '/', function(req, res){
 
 // List of databases
 app.get(basepath + '/db', blastdbcmd.getDBlist);
-// List of species
-app.get(basepath + '/species', queries.getListSpecies);
-
-// TODO: Also get a list of entries
-app.get(basepath + '/db/:db/taxon/:taxon/fasta', blastdbcmd.getBlastDBcmdTaxon);
 
 // TODO: Multiple sequences at one
 // All possibilities (range is xx-yy)
-app.get(basepath + '/fasta/:fmt/:entry/:db/:range/:line', blastdbcmd.getBlastDBcmd);
-app.get(basepath + '/fasta/:fmt/:entry/:db/:range', blastdbcmd.getBlastDBcmd);
-app.get(basepath + '/fasta/:fmt/:entry/:db', blastdbcmd.getBlastDBcmd);
-app.get(basepath + '/fasta/:fmt/:entry', blastdbcmd.getBlastDBcmd);
 
-app.post(basepath + '/fasta', blastdbcmd.getBlastDBcmd);
-
-// Alternate
 app.get(basepath + '/db/:db/entry/:entry/fasta', blastdbcmd.getBlastDBcmd);
 app.get(basepath + '/db/:db/entry/:entry/fasta/:fmt', blastdbcmd.getBlastDBcmd);
 app.get(basepath + '/db/:db/entry/:entry/fasta/:fmt/:range', blastdbcmd.getBlastDBcmd);
@@ -73,24 +59,9 @@ app.get(basepath + '/db/:db/entry/:entry/fasta/:fmt/:range/:line', blastdbcmd.ge
 
 // Retrieval by POST
 app.post(basepath + '/db', blastdbcmd.getBlastDBcmd);
-// Retrieval by POST with SMW filtering
-app.post(basepath + '/smwdb', wiki.getSMWBlastDBcmd);
-
-// Get png file of gene models
-app.get(basepath + '/genemodels/:taxonid/:chromosome/:start/:gene', function (req, res) {
-    var st0 = req.params.start;
-    var stf = st0.substring(0, 1);
-    res.sendfile('/home/andreu/genemodels/' + req.params.taxonid + '/' + req.params.chromosome + '/' + stf + '/' + req.params.gene);
-});
 
 // Blast
 app.post(basepath + '/blast', blast.performBlast);
-
-// Transcript
-app.get(basepath + '/transcript/:transcript', queries.getWholeTranscriptInfo);
-// Transcript - short (used in BLAST)
-app.get(basepath + '/transcript/:transcript/short', queries.getTranscriptInfo);
-
 
 // Now views
 app.set('views', __dirname + '/views');
