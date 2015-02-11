74c6df4729fcd8d2aa4a63cd26be57b396bac9e3
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-11 21:46:06 +0100

    Update with blast

--- public/js/blast.js
+++ public/js/blast.js
@@ -1,9 +1,52 @@
-/*global io, console, location $ */
+/*globals console io $ document */
 
-var socket = io.connect(); // TODO: Check this!
+var socket = io.connect();
 socket.on('output', function(message) { console.log('output: ', message); $("#blast-data").append( message ); });
 
+$(document).ready( function(){
+
+	var basepath = $("blast-form").data("basepath");
+
+	$.get( basepath + "/api/db", function( data ) {
+
+		if ( data ) {
+			if ( data.nucl ) {
+				for ( var k in data.nucl ) {
+					if ( data.nucl[k] ) {
+						$("#nucl").append("<option>" + k + "</option>" );
+					}
+				}
+			}
+			if ( data.prot ) {
+				for ( var k in data.prot ) {
+					if ( data.prot[k] ) {
+						$("#prot").append("<option>" + k + "</option>" );
+					}
+				}
+			}
+		}
+	});
+
+	// Getting organisms
+	$("[name=organism]").append("<option value='0'>" + "All" + "</option>" );
+	$.get( basepath + "/api/species", function( data ) {
+		if ( data ) {
+			if (data instanceof Array) {
+				for ( var k in data ) {
+					if ( data[k] ) {
+						$("[name=organism]").append("<option value="+data[k].id+">" + data[k]["scientific_name"] + "</option>" );
+					}
+				}
+			} else {
+				$("[name=organism]").append("<option value="+data.id+">" + data["scientific_name"] + "</option>" );
+			}
+		}
+	});
+
+});
+
 $( "[name=moltype]" ).change(function() {
+
 	var valid = "#" + $(this).val();
 	$( ".dbselect" ).hide();
 	$( valid ).show();
@@ -11,10 +54,13 @@ $( "[name=moltype]" ).change(function() {
 });
 
 $(function() {
-	$("button").click(function() {
+	$('#blast-exec').click(function() {
+
 		var exec = $(this).attr("data-blast-exec");
 		var binary = null;
 		var db = null;
+		var format = "html";
+		var organism = 0;
 
 		if ( $( "[name=moltype]" ).val() === 'nucl' ) {
 			binary = "blastn";
@@ -24,6 +70,29 @@ $(function() {
 			db = $( "[name=protlist]" ).val();
 		}
 		
-		$.post( exec, { seq: $('textarea').val(), binary: binary, db: db });
+		organism = parseInt( $( "[name=organism]" ).val(), 10 );
+
+		$("#blast-data").empty();
+		$.post( exec, { seq: $('textarea').val(), binary: binary, db: db, format: format, organism: organism });
 	});
+});
+
+// Detect details
+$( document ).on( "click", ".hit > .details", function() {
+
+	var hsps = $(this).parent().children(".hsps").get(0);
+
+	$(hsps).fadeToggle('fast');
+
+});
+
+// Detect hit appears
+$(document).on('DOMNodeInserted', function(e) {
+
+	if ( $(e.target).hasClass("hit") ) {
+		
+		var idelem = $(e.target).children(".id").get(0);
+		var id = $(idelem).text();
+		// TODO: Link here to Database or system
+	}
 });
