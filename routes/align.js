ca5ee6ea1c784a137ea46a3a5b462f2c0f433398
toniher <toniher@cau.cat>
2015-08-24 19:17:07 +0200

    add align

--- routes/align.js
+++ routes/align.js
@@ -11,12 +11,49 @@ exports.performAlign = function (req, res) {
 	config = req.app.set('config');
 
 	var alnparams = {};
-	// Here alignment params
-	// Application programs to use
-	// e.g. muscle, others
-	// Sequences
-	// if tree processing
+
+	alnparams.seq = req.body.seq;
+	alnparams.align.app = req.body.align.app;
+	alnparams.align.params = req.body.align.params;
+	alnparams.tree.app = req.body.tree.app;
+	alnparams.tree.params = req.body.tree.params;
+	alnparams.treeview.app = req.body.treview.app;
+	alnparams.treeview.params = req.body.treview.params;
+
+	// Generate FASTA file
+
+	// Run alignment from input file
+	
+
+	// Run tree
+
+	// Run treeview
 
 	// TODO process
 
 };
+
+function run_cmd ( args, callBack ) {
+
+	var resp = "";
+	
+	// Elements to pipe
+	var textfile = [ ">ENTRY", args[0] ].join("\n");
+	var blastprog = args[1] + " -db " + args[2] + " -outfmt 13 " + args[3];
+
+	// We pass thru STDOUT, we avoid temp file
+	// Handled by procstreams
+	$p("echo \"" + textfile + "\"" ).pipe( blastprog )
+	.data(function(err, stdout, stderr) {
+		if ( err ) {
+			console.log("ERR");
+			console.log( stderr.toString() );
+		} else {
+			// console.log("OUT");
+			resp += stdout.toString();
+			callBack(resp);
+		}
+	
+	});
+
+}
