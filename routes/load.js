2e9eded3c472fa72f9fd620e1c5152be6c150f09
toniher <toniher@cau.cat>
2016-03-11 12:01:18 +0100

    starting load module

---
+++ routes/load.js
@@ -0,0 +1,11 @@
+var functions = require('../functions/index.js');
+var temp = require('temp'),
+    fs   = require('fs');
+
+// Retrieve a File
+exports.getFile = function (req, res) {
+
+	var config;
+	config = req.app.set('config');
+
+};
