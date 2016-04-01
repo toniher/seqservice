208075b70ae5599f05eb46542cb269e4d04ea934
Toni Hermoso Pulido <toniher@cau.cat>
2016-04-02 01:21:08 +0200

    BLAST and bypass

--- public/js/extern/service.js
+++ public/js/extern/service.js
@@ -1,7 +1,23 @@
+$(document).ready( function(){
+
+	var basepath = $("body").data("basepath");
+	var socketio = $("body").data("socketio");
+
+
+	if ( socketio ) {
+		var socket = io.connect( { path: basepath + "/socket.io" } );
+		socket.on('service', function(message) {
+			prepareHTMLService( message );
+		});
+	}
+	
+});
+
 $(document).on('click', ".service-exec", function() {
 
 	var basepath = $("body").data("basepath");
-
+	var socketio = $("body").data("socketio");
+		
 	// Get service
 	if ( $(this).data('id') ) {
 		
@@ -43,19 +59,52 @@ $(document).on('click', ".service-exec", function() {
 					params.input = JSON.stringify( obj.data ); // Otherwise we lose types
 					
 					$.post( basepath+"/service", { params: params }).done( function( data ) {
-			
-						console.log( data );
-						// Data here and save
+						
+						if ( ! socketio ){
+							prepareHTMLService( data );
+						}
 			
 					});
 				}
 			});
 
 		}
-		
-		
-
 	
 	}
 
 });
+
+
+function prepareHTMLService( message ) {
+
+	console.log( message );
+
+	var obj = JSON.parse( message );
+	
+	if ( obj.type == 'bypass' ) {
+
+		
+		printBypass( obj, 0, function( txt, extra ) {
+			// Handle extra iter
+			console.log( extra );
+		});
+	
+	}
+	
+}
+
+function printBypass( message, parse, target ) {
+	
+	var obj;
+	if ( parse ) {
+		obj = JSON.parse( message );
+	} else {
+		obj = message;
+	}
+	
+	console.log( obj );
+	
+	// print -> Retrieve BLAST and retrieve Bypass
+	
+}
+
