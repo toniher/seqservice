84cf6270af2686da076dd49630194988483fb708
Toni Hermoso Pulido <toniher@cau.cat>
2016-07-27 00:30:21 +0200

    file exists

--- routes/down.js
+++ routes/down.js
@@ -3,8 +3,8 @@ var temp = require('temp'),
     fs   = require('fs');
 	
 
-exports.getTemp = function(req, res) {
-
+exports.checkExists = function( req, res ) {
+	
 	var config = req.app.set('config');
 	
 	var directory = false;
@@ -13,8 +13,6 @@ exports.getTemp = function(req, res) {
 	var contenttype = "text/x-fasta";
 	var filename = "down.fasta";
 	
-	var stdout = "";
-
 	if ( req.body.directory ) {
 		directory = req.body.directory;
 	}
@@ -35,14 +33,81 @@ exports.getTemp = function(req, res) {
 		filename = req.body.filename;
 	}
 
-
-	var contentdisposition = "attachment; filename=" + filename;
-
+	// var contentdisposition = "attachment; filename=" + filename;
 
 	res.set( 'Content-Type', contenttype );
-	res.set( 'Content-Disposition', contentdisposition );
+	
+	var filepath = "/tmp" + path;
+	
+	
+	var outcome = {};
+	outcome.exists = false;
+	
+	fs.exists(filepath, function(exists) {
+	    if (exists) {
+			outcome.exists = true;
+			functions.returnJSON( res, outcome );
+		} else {
+			functions.returnJSON( res, outcome );
+		}
+	});	
+	
+};
 
-	res.send( stdout );
+exports.getTemp = function(req, res) {
+
+	var config = req.app.set('config');
+	
+	var directory = false;
+	var compress = false;
+	var path = false;
+	var contenttype = "text/x-fasta";
+	var filename = "down.fasta";
+	
+	if ( req.body.directory ) {
+		directory = req.body.directory;
+	}
+
+	if ( req.body.compress ) {
+		compress = req.body.compress;
+	}
+	
+	if ( req.body.path ) {
+		path = req.body.path;
+	}
+	
+	if ( req.body.contenttype ) {
+		contenttype = req.body.contenttype;
+	}
+
+	if ( req.body.filename ) {
+		filename = req.body.filename;
+	}
+
+	// var contentdisposition = "attachment; filename=" + filename;
+
+	res.set( 'Content-Type', contenttype );
+	
+	var filepath = "/tmp" + path;
+	
+	
+	fs.exists(filepath, function(exists) {
+	    if (exists) {
+			// Do something
+			res.download( filepath, filename, function(err){
+				if (err) {
+				  // Handle error, but keep in mind the response may be partially-sent
+				  // so check res.headersSent
+				  console.err("Some error!");
+				}
+			});
+	    }
+	});
+	
+
+	//res.set( 'Content-Disposition', contentdisposition );
+
+	// res.send( stdout );
 
 
 };
