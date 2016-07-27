8fbb1612fd02b7b097b6fe072b4456031088f7a6
toniher <toniher@cau.cat>
2016-07-27 17:09:01 +0200

    download system

--- public/js/blast.js
+++ public/js/blast.js
@@ -270,7 +270,7 @@ $(document).on('click', ".down-hit-seqs", function() {
 					$("#down-form").empty();
 
 					if ( response.path ) { $("#down-form").append("<input name='path' type='hidden' value='"+response.path+"' />"); }
-					if ( response.filename ) { $("#down-form").append("<input name='filename' type='hidden' value='"+response.filename+"' />"); }
+					if ( response.filename ) { $("#down-form").append("<input name='filename' type='hidden' value='"+response.filename+".fasta' />"); }
 					$("#down-form").append("<input id='down-button' class='btn btn-primary' type='submit' value='Download' />");
 				}
 			}
