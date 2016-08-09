7ae44a94da31e982ccf2538f0e78472daad6bc2d
toniher <toniher@cau.cat>
2016-08-09 18:07:02 +0200

    adding link

---
+++ routes/link.js
@@ -0,0 +1,34 @@
+var functions = require('../functions/index.js');
+var temp = require('temp'),
+    fs   = require('fs');
+
+exports.getLinks = function(req, res) {
+
+	var config = req.app.set('config');
+
+	var outcome = {};
+
+	var db = null;
+	if ( req.params.db ) {
+		db = req.params.db;
+	}
+
+	var dbtype = null;
+
+	if ( req.params.dbtype ) {
+		dbtype = req.params.dbtype;
+	}
+
+	if ( db && dbtype) {
+		if ( config[ dbtype ] && config[ dbtype ][ db ] && config[ dbtype ][ db ]["links"] ) {
+			outcome = config[ dbtype ][ db ]["links"];
+		}
+
+	} 
+	
+	functions.returnJSON( res, outcome );
+
+};
+
+
+
