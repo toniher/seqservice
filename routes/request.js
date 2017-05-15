33f631cfcb65df2db5cbed5bacba7ad61478e208
Toni Hermoso Pulido <toniher@cau.cat>
2017-05-15 18:40:18 +0200

    adding request starting point

---
+++ routes/request.js
@@ -0,0 +1,13 @@
+var functions = require('../functions/index.js');
+var temp = require('temp').track(),
+    fs   = require('fs');
+
+require('babel-polyfill');
+var hash = require('json-hash');
+var moment = require('moment');
+
+exports.prepareRequest  = function (req, res) {
+	
+};
+
+
